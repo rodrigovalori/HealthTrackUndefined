fd70269f3389ac2e3e1d450042fd4b071e3c7d6b
rodrigovalori <rodrigo.valori@avenuecode.com>
2021-11-22 23:07:11 -0300

    removing usuario.class unused methods

--- src/main/java/br/com/fiap/healthtrack/business/usuario/UsuarioBusiness.java
+++ src/main/java/br/com/fiap/healthtrack/business/usuario/UsuarioBusiness.java
@@ -1,8 +1,5 @@
 package br.com.fiap.healthtrack.business.usuario;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.fiap.healthtrack.dao.UsuarioDAO;
 import br.com.fiap.healthtrack.model.Usuario;
 
@@ -11,56 +8,6 @@ public class UsuarioBusiness {
 	public void cadastrar(Usuario u) {
 		UsuarioDAO udao = new UsuarioDAO();
 		udao.add(u);
-	}
-
-	public List<Usuario> listarTodos() {
-
-		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
-
-		UsuarioDAO udao = new UsuarioDAO();
-		listaUsuarios = udao.getAll();
-
-		return listaUsuarios;
-	}
-
-	public Usuario pesquisarPorId(int codigo) {
-
-		Usuario Usuario = new Usuario();
-		UsuarioDAO udao = new UsuarioDAO();
-		Usuario = udao.getById(codigo);
-
-		return Usuario;
-	}
-	
-	public Usuario pesquisarIdPorEmail(String email) {
-
-		Usuario usuario = new Usuario();
-		UsuarioDAO udao = new UsuarioDAO();
-		usuario = udao.getIdByEmail(email);
-
-		return usuario;
-	}
-
-	public void editar(Usuario u) {
-
-		UsuarioDAO udao = new UsuarioDAO();
-		udao.update(u);
-	}
-
-	public void excluir(int codigo) {
-
-		UsuarioDAO udao = new UsuarioDAO();
-		udao.delete(codigo);
-	}
-
-	public Usuario validarEmailESenha(String email, String senha) {
-		Boolean loginPermitido = false;
-		Usuario usuario = new Usuario();
-		UsuarioDAO udao = new UsuarioDAO();
-		usuario = udao.getIdByEmailAndPassword(email, senha);
-		System.out.println("BUSCA: " + usuario);
-		
-		return usuario;
 	}
 
 }
